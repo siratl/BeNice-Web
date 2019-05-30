2e7ef88309cd17d7b0d79c316c7862d0d2d5fec5
siratl <elishubax@gmail.com>
2019-05-29 22:59:43 -0700

    Initial commit: Created react app

---
+++ benice-essentials/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section>
+          <h1>BNE Island</h1>
+          <nav>HOME</nav>
+        </section>
+
+        <section>
+          <h2>Welcome to the BNE Island</h2>
+          <p>Lobby of BeNice Essentials online precence.</p>
+          <p>Please select a branch to visit below.</p>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
