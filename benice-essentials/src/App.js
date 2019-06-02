9f7a9544662c4c8f8033d460e2a5cb61145f79ae
siratl <elishubax@gmail.com>
2019-06-01 23:43:30 -0700

    added buttons, footer and navbar styles

--- benice-essentials/src/App.js
+++ benice-essentials/src/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" />
+
+      <div className="App-content">
         <section className="header">
           <h1>BNE Island</h1>
           <nav>HOME</nav>
         </section>
 
         <section className="content">
-          <h2>Welcome to the BNE Island</h2>
-          <p>Lobby of BeNice Essentials online precence.</p>
+          <h3>Nature | Nurture | Growth</h3>
+          <p>
+            Welcome to the BNE Island. The one stop shop for everything BeNice
+            Essentials, your newest obsession.
+          </p>
           <p>Please select a branch to visit below.</p>
         </section>
-      </header>
+
+        <section className="button-Btn">
+          <Button color="primary" size="2x">
+            Shop Now
+          </Button>
+          <Button color="primary" size="2x">
+            Facebook
+          </Button>
+          <Button color="primary" size="2x">
+            Instagram
+          </Button>
+          <Button color="secondary" size="2x" disabled>
+            Blog
+          </Button>
+        </section>
+      </div>
+
+      <footer className="footer-content">
+        <p>&copy; Copyright 2019 BeNice Essentials - All Right Reserved</p>
+      </footer>
     </div>
   );
 }
