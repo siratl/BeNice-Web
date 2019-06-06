fc0b29d735e6fc1b7b2785a658dc280075270509
Elisha Atulomah <elishaubax@gmail.com>
2019-06-06 10:40:42 -0700

    added mobile media query and background image

--- benice-essentials/src/App.js
+++ benice-essentials/src/App.js
@@ -7,59 +7,54 @@ function App() {
   return (
     <div className="App">
       <header className="App-header" />
-
       <div className="App-content">
         <section className="header">
           <h1>BNE Island</h1>
         </section>
-
         <div className="headerImage">
           <img src={headerImage} alt="" />
           <section className="content">
             <h3>Nature | Nurture | Growth</h3>
             <p>
-              Welcome to the BNE Island. The one stop shop for everything BeNice
-              Essentials, your newest obsession.
+              Welcome to your newest obsession, BNE Island. The one stop shop
+              for everything BeNice Essentials!
             </p>
-            <p>Please select a branch to visit below.</p>
+            <section className="button-Btn">
+              <Button
+                href="https://www.beniceessentials.com"
+                target="_blank"
+                id="isActive"
+                color="primary"
+                size="2x"
+              >
+                Shop Now
+              </Button>
+              <Button
+                href="https://www.facebook.com/beniceessentials/"
+                target="_blank"
+                id="isActive"
+                color="primary"
+                size="2x"
+              >
+                Facebook
+              </Button>
+              <Button
+                href="https://www.instagram.com/beniceessentials/"
+                target="_blank"
+                id="isActive"
+                color="primary"
+                size="2x"
+              >
+                Instagram
+              </Button>
+              <Button id="tooltip" color="secondary" size="2x" disabled>
+                Blog
+                <span class="tooltiptext">Coming soon!</span>
+              </Button>
+            </section>
           </section>
         </div>
-
-        <section className="button-Btn">
-          <Button
-            href="https://www.beniceessentials.com"
-            target="_blank"
-            id="isActive"
-            color="primary"
-            size="2x"
-          >
-            Shop Now
-          </Button>
-          <Button
-            href="https://www.facebook.com/beniceessentials/"
-            target="_blank"
-            id="isActive"
-            color="primary"
-            size="2x"
-          >
-            Facebook
-          </Button>
-          <Button
-            href="https://www.instagram.com/beniceessentials/"
-            target="_blank"
-            id="isActive"
-            color="primary"
-            size="2x"
-          >
-            Instagram
-          </Button>
-          <Button id="tooltip" color="secondary" size="2x" disabled>
-            Blog
-            <span class="tooltiptext">Coming soon!</span>
-          </Button>
-        </section>
       </div>
-
       <footer className="footer-content">
         <p>&copy; Copyright 2019 BeNice Essentials - All Right Reserved</p>
       </footer>
