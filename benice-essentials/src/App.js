761482541d14e0f6e8d135adfc389b6cd6d8c841
Elisha <elishaubax@gmail.com>
2020-05-04 21:26:46 -0700

    updated dependencies and added Blog link to jsx

--- benice-essentials/src/App.js
+++ benice-essentials/src/App.js
@@ -6,57 +6,62 @@ import headerlogo from './images/Benice-Favicon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h2>BeNice Essentials</h2>
       </header>
-      <div className="App-content">
-        <section className="header">
+      <div className='App-content'>
+        <section className='header'>
           <h1>BNE Island</h1>
         </section>
-        <div className="headerImage">
-          <img src={headerImage} alt="BeNice Essentials products" />
+        <div className='headerImage'>
+          <img src={headerImage} alt='BeNice Essentials products' />
 
-          <section className="content">
+          <section className='content'>
             <h3>Nature | Nurture | Growth</h3>
             <p>Welcome to your newest obsession. Where would you like to go?</p>
-            <section className="button-Btn">
+            <section className='button-Btn'>
               <Button
-                href="https://www.beniceessentials.com"
-                target="_blank"
-                id="isActive"
-                color="primary"
-                size="2x"
+                href='https://www.beniceessentials.com'
+                target='_blank'
+                id='isActive'
+                color='primary'
+                size='2x'
               >
                 Shop Now
               </Button>
               <Button
-                href="https://www.facebook.com/beniceessentials/"
-                target="_blank"
-                id="isActive"
-                color="primary"
-                size="2x"
+                href='https://www.facebook.com/beniceessentials/'
+                target='_blank'
+                id='isActive'
+                color='primary'
+                size='2x'
               >
                 Facebook
               </Button>
               <Button
-                href="https://www.instagram.com/beniceessentials/"
-                target="_blank"
-                id="isActive"
-                color="primary"
-                size="2x"
+                href='https://www.instagram.com/beniceessentials/'
+                target='_blank'
+                id='isActive'
+                color='primary'
+                size='2x'
               >
                 Instagram
               </Button>
-              <Button id="tooltip" color="secondary" size="2x" disabled>
+              <Button
+                href='https://beniceessentials.com/bne-blog/'
+                target='_blank'
+                id='isActive'
+                color='primary'
+                size='2x'
+              >
                 Blog
-                <span className="tooltiptext">Coming soon!</span>
               </Button>
             </section>
           </section>
         </div>
       </div>
-      <footer className="footer-content">
+      <footer className='footer-content'>
         <p>&copy; Copyright 2019 BeNice Essentials - All Rights Reserved</p>
       </footer>
     </div>
